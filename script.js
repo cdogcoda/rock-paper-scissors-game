8658d8aacdb18ed228c7299b0b05352b02031cce
CDog <cdog97456@gmail.com>
2024-01-13 16:07:15 -0800

    Add pseudocode steps (provided by TOP)

---
+++ script.js
@@ -0,0 +1,9 @@
+// TOP's recommended steps
+
+// Create a function getComputerChoice that randomly returns rock, paper, or scissors (using some randomized function)
+// This should return and represent the computer's choice in each round of a game
+
+// Create a function playRound that takes two parameters (playerSelection, computerSelection) to play a round of the game
+// This should return a string declaring who won (should be case-insensitive, account for ties by playing the round again (recalling func.?))
+
+// Create an outer function game that runs playRound five times, keeps track of score, and reports the winner after
