8d075aca32166b4f94c344787592d50aa45508f6
CDog <cdog97456@gmail.com>
2024-01-13 16:08:11 -0800

    Create function getComputerChoice

--- script.js
+++ script.js
@@ -3,6 +3,21 @@
 // Create a function getComputerChoice that randomly returns rock, paper, or scissors (using some randomized function)
 // This should return and represent the computer's choice in each round of a game
 
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch (computerChoice) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+}
+
 // Create a function playRound that takes two parameters (playerSelection, computerSelection) to play a round of the game
 // This should return a string declaring who won (should be case-insensitive, account for ties by playing the round again (recalling func.?))
 
